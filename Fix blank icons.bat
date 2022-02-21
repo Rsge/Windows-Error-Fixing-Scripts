84481a1c543ec0a48a93515484b51c2c26284a93
Jan <rsge@web.de>
2022-02-21 15:46:46 +0100

    Trimmed a few imo unnecessary echo.s

--- Fix blank icons.bat
+++ Fix blank icons.bat
@@ -4,24 +4,19 @@ set iconcache_x=%localappdata%\Microsoft\Windows\Explorer\iconcache*
 
 echo.
 echo The explorer process must be temporarily killed before deleting the IconCache.db file.
-echo.
 echo Please SAVE ALL OPEN WORK before continuing.
 echo.
 pause
 echo.
 If exist "%iconcache%" goto delete
-echo.
 echo The %localappdata%\IconCache.db file has already been deleted.
 echo.
 If exist "%iconcache_x%" goto delete
-echo.
 echo The %localappdata%\Microsoft\Windows\Explorer\IconCache_*.db files have already been deleted.
 echo.
 exit /B
 
-
 :delete
-echo.
 echo Attempting to delete IconCache.db files...
 echo.
 ie4uinit.exe -show
@@ -29,16 +24,9 @@ taskkill /IM explorer.exe /F
 If exist del /A /F /Q "%iconcache%"
 If exist del /A /F /Q "%iconcache_x%"
 start explorer.exe
-echo.
 echo IconCache database files have been successfully deleted.
-goto restart
-
-
-:restart
-echo.
 echo.
 echo You will need to restart the PC to finish rebuilding your icon cache.
-echo.
 CHOICE /C:YN /M "Do you want to restart the PC now?"
 IF ERRORLEVEL 2 goto no
 IF ERRORLEVEL 1 goto yes
