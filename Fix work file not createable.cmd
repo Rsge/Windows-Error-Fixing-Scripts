77201ea5ce2032e4ea4b9aa2ff36287ef7c0a736
JG <it@hgw-tornesch.de>
2023-01-10 08:53:30 +0100

    Fixed inconsistent backup folder & un-escaped ampersand

--- Fix work file not createable.cmd
+++ Fix work file not createable.cmd
@@ -30,8 +30,8 @@ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul & shift /1)
 REM Functional part
 echo Create backups of registry entries to delete in Downloads folder. . .
 reg export "HKCR\CLSID\{84F66100-FF7C-4fb4-B0C0-02CD7FB668FE}" "%userprofile%\Downloads\SBLOG_RegBackup_WordPreview.reg"
-reg export "HKCR\CLSID\{65235197-874B-4A07-BDC5-E65EA825B718}" "%userprofile%\documents\SBLOG_RegBackup_PowerPointreview.reg"
-reg export "HKCR\CLSID\{00020827-0000-0000-C000-000000000046}" "%userprofile%\documents\SBLOG_RegBackup_ExcelPreview.reg"
+reg export "HKCR\CLSID\{65235197-874B-4A07-BDC5-E65EA825B718}" "%userprofile%\Downloads\SBLOG_RegBackup_PowerPointreview.reg"
+reg export "HKCR\CLSID\{00020827-0000-0000-C000-000000000046}" "%userprofile%\Downloads\SBLOG_RegBackup_ExcelPreview.reg"
 echo.
 echo Deleting entries. . .
 reg delete "HKCR\CLSID\{84F66100-FF7C-4fb4-B0C0-02CD7FB668FE}" /f
@@ -45,6 +45,6 @@ echo.
 echo Defining new registry entries for chache path. . .
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "Cache" /t REG_SZ /d "%userprofile%\AppData\Local\Microsoft\Windows\INetCache" /f
 echo.
-echo Done. Please log off & on and try if the problem was resolved.
+echo Done. Please log off ^& on and try if the problem was resolved.
 pause
 exit
